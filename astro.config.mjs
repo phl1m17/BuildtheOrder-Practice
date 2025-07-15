4a4b97b53d5500cbf5285ebc599ac67bba09d46a
phl1m17 <clarkmanag@gmail.com>
2025-07-15 10:42:11 -0400

    asd

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,6 @@ import react from "@astrojs/react";
 // https://astro.build/config
 export default defineConfig({
   base: "/BuildtheOrder-Practice/",
+  outDir: "docs",
   integrations: [react()],
 });
